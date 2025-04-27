d97e9d38b0001fd6b7664534e6b2563fca1402fd
Y.P. Liu <ypliu18@gmail.com>
2025-04-27 19:11:56 +0800

    Update subscription id

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,7 +4,7 @@ import categories from './categories';
 import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
-  id: 233,
+  id: 9587254467,
   name: 'Subscription',
   version: 0,
   author: 'author',
